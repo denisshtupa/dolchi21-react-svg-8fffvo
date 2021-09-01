825ea86a67841782dc124cdb7a04e11349105006
denisshtupa <denis.shtupa@gmail.com>
2021-09-01 11:50:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import Finance from './components/Finance';
+import Growth from './components/Growth';
+import Login from './components/Login';
+
+import './style.css';
+
+var SVGs = [Finance, Login, Growth];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {SVGs.map(C => (
+          <C size={200} />
+        ))}
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
